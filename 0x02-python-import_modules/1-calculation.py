a4c43a73feaffc0e5c5b61297bd5a585b00bce34
PlexSir <khayak3@gmail.com>
2023-08-11 17:19:38 -0700

    alx-higher_level_programming

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Display the addition, subtraction, multiplication, and division results of 10 and 5."""
+    """The sum, subtraction, multiplication, and division results of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
