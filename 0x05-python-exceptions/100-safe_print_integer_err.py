e7347ad5fda35b3760175401aaf74aeab7338e84
PlexSir <khayak3@gmail.com>
2023-08-28 15:57:15 -0700

    alx-higher_level_programming

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format().
+    If ValueError is caught, message is printed.
+    Args:
+        value (int): The integer to print.
+    Returns:
+        If TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
