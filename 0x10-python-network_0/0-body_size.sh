6a28d3ba54ca0b30d621cdc6062687b0ec287693
PlexSir <88951611+PlexSir@users.noreply.github.com>
2024-01-26 01:46:17 +0200

    0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# Check if the URL is provided as an argument
-if [ $# -ne 1 ]; then
+# Check if the URL was provided as an argument
+if [ -z "$1" ]; then
   echo "Usage: $0 <URL>"
   exit 1
 fi
 
-# Store the URL provided as an argument
-url=$1
+# Send the request using curl and save the response body to a variable
+response=$(curl -s -D - "$1" | grep -E '^Content-Length: [0-9]+')
 
-# Send a GET request to the URL and store the response body in a variable
-response=$(curl -s -w "%{size_download}" -o /dev/null $url)
+# Extract the content length (response size in bytes) from the response header
+content_length=$(echo "$response" | awk '{print $2}')
 
-# Display the size of the response body in bytes
-echo $response
+# Display the content length
+if [ -n "$content_length" ]; then
+  echo "Response size in bytes: $content_length"
+else
+  echo "Failed to retrieve the response size."
+  exit 1
+fi
