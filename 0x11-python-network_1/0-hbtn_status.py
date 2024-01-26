ca1f5477c8706a021ede857bed5834cdceaaacd0
PlexSir <88951611+PlexSir@users.noreply.github.com>
2024-01-26 05:12:57 +0200

    alx-higher_level_programming

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,4 +4,5 @@ import urllib.request
 url = "https://alx-intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as response:
     content = response.read().decode('utf-8')
+print("-" * 10 + " BODY " + "-" * 10)
 print(content)
