a0d428ed51b70e10219eba2e0db1f91259a74218
PlexSir <88951611+PlexSir@users.noreply.github.com>
2024-01-26 05:23:27 +0200

    alx-higher_level_programming

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-
+"""Fetch https://alx-intranet.hbtn.io/status."""
 import urllib.request
-url = "https://alx-intranet.hbtn.io/status"
-with urllib.request.urlopen(url) as response:
-    content = response.read().decode('utf-8')
-print("-" * 10 + " BODY " + "-" * 10)
-print(content)
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
